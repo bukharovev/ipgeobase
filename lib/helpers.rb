254493f39905c9659d9f80f8e4df4c55cdcea263
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-06-16 23:21:17 +0300

    wrap to class

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,15 +2,18 @@
 
 require_relative 'config'
 
-def build_api_url(ip_address)
-  template = Addressable::Template.new("#{API_URL}/{path}/{params}{?query*}")
-  template.expand(
-    {
-      path: 'json',
-      params: ip_address,
-      query: {
-        fields: 'city,country,countryCode,lat,lon'
+# class Helper
+class Helper
+  def self.build_api_url(ip_address)
+    template = Addressable::Template.new("#{API_URL}/{path}/{params}{?query*}")
+    template.expand(
+      {
+        path: 'json',
+        params: ip_address,
+        query: {
+          fields: 'city,country,countryCode,lat,lon'
+        }
       }
-    }
-  )
+    )
+  end
 end
